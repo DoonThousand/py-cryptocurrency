8203fde08e5a80c5984a16ec0a920417757cf40a
Admin <valerasarkisan285@gmail.com>
2026-05-17 17:55:50 +0300

    py-cryptocurrency

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,24 @@
-# write your code here
+from unittest.mock import patch
+
+from app.main import cryptocurrency_action
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_high(mock_current_rate: int) -> None:
+    mock_current_rate.return_value = 16
+    result = cryptocurrency_action(10)
+    assert result == "Buy more cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_low(mock_current_rate: int) -> None:
+    mock_current_rate.return_value = 9
+    result = cryptocurrency_action(10)
+    assert result == "Sell all your cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_final(mock_current_rate: int) -> None:
+    mock_current_rate.return_value = 9.5
+    result = cryptocurrency_action(10)
+    assert result == "Do nothing"
